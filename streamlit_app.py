7f29fd2748d59e9229c1791b3da96209ba62fcd6
Almirah1 <almirahm1@gmail.com>
2022-05-02 09:43:01 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.title ('My Mom\'s New Healthy Diner')
 
 streamlit.header ('Breakfast menu')
 streamlit.text ('Omega 3 & Blueberry Oatmeal')
+streamlit.text ('Kale, Spinach and Rocket Smoothie ')
+streamlit.text ('Hard-Boiled Free-Range Egg')
